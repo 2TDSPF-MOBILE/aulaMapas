dc64a6fdb8c41998401335a66b140a98482bb17c
Prof. Fernando Pinéo <pf1963@fiap.com.br>
2025-05-13 11:55:22 -0300

    Realizado a segunda parte do mapa

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,Button } from 'react-native';
 import React,{useState,useEffect} from 'react';
-
+import RNPickerSelect from 'react-native-picker-select'
 //Biblioteca de localização
 import * as Location from 'expo-location'
 
 //Importação do Mapas
 import MapView,{Marker} from 'react-native-maps'
 
+const nomeAlunos = [
+  {label:'Abner',value:'Abner'},
+  {label:'Luiz',value:'Luiz Paulo'},
+  {label:'Isabella',value:'Isabella'}
+]
+
 export default function App() {
+  const[alunoSelecionado,setAlunoSelecionado]=useState('')
   // Estado para armazenar localizaçãp (coordenadas->Latitude e Longitude)
   const[location,setLocation]=useState(null)
 
@@ -42,7 +49,7 @@ export default function App() {
         )
       }
 
-    })
+    })()//Função assincrona auto executável
   },[])
 
   //Função para exibir o endereço
@@ -58,20 +65,56 @@ export default function App() {
     return(
       <View>
         <Text>Endereço Completo</Text>
-        <Text>{street}</Text>
-        <Text>{city}{region}</Text>
-        <Text>{postalCode}</Text>
-        <Text>{country}</Text>
+        <Text>Rua:{street}</Text>
+        <Text>Cidade:{city}</Text>
+        <Text>Estado:{region}</Text>
+        <Text>CEP:{postalCode}</Text>
+        <Text>País:{country}</Text>
       </View>
     )
   }
 
-  console.log(address)
-
+  
   return (
     
     <View style={styles.container}>
-      {renderAddress()}
+      <Text>Sua localização</Text>
+      {location?(
+        <>
+          <Text>Latitude:{location.latitude}</Text>
+          <Text>Longitude:{location.longitude}</Text>
+
+          <MapView
+            style={{width:'100%',height:400}}
+            initialRegion={{
+              latitude:location.latitude,
+              longitude:location.longitude,
+              latitudeDelta:0.01,//zoom vertical
+              longitudeDelta:0.01//zom horizontal
+            }}
+          >
+             <Marker 
+              coordinate={{
+                latitude:location.latitude,
+                longitude:location.longitude
+              }}
+              title='Você está aqui'
+            />
+
+          </MapView>
+          {renderAddress()}
+          <RNPickerSelect 
+            items={nomeAlunos}
+            onValueChange={(value)=>setAlunoSelecionado(value)}
+            placeholder={{label:'Selecione o aluno',value:null}}
+          />
+          {alunoSelecionado?(
+            <Text>Aluno selecionado foi:{alunoSelecionado}</Text>
+          ):null}
+        </>
+      ):(
+        <Text>Carregando endereço...</Text>
+      )}
     </View>
   );
 }
@@ -81,6 +124,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:40
   },
 });
